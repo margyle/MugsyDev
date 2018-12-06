f86722b29e0f8c8ea1e060c98b6d9fbc4e0a1349
Matt Argyle <matt.coding@gmail.com>
2018-12-05 19:07:25 -0500

    Update models.py

--- decaf/models.py
+++ decaf/models.py
@@ -1,7 +1,11 @@
-from extensions import mysql
+import sys
+import os
+from decafConfig import mysql
+sys.path.insert(0,'helpers')
+from barcodeHelper import scan, duringScan, afterScan
 
 
-class requestBrewSettings:
+class BrewSettings:
 	def get(a):
 		user = a		
 		cur = mysql.connect().cursor()
@@ -10,7 +14,7 @@ class requestBrewSettings:
 			for i, value in enumerate(row)) for row in cur.fetchall()]
 		return {'brewSettings' : r}
 
-class getCoffeeInfo:
+class CoffeeInfo:
 	def get(a):
 		coffeeTypeId = a		
 		cur = mysql.connect().cursor()
@@ -18,6 +22,32 @@ class getCoffeeInfo:
 		r = [dict((cur.description[i][0], value)
 			for i, value in enumerate(row)) for row in cur.fetchall()]
 		return {'brewSettings' : r}
+
+class barcodeScanner:
+	def get(self):
+		count = 1
+		while(count < 12):
+			contents = scan()
+			count = len(contents)
+			duringScan()
+		else:  
+			for line in open("/var/mugsy/decaf/helpers/upc.txt"):
+				last=line
+				barcode = [last.rstrip('\n')]
+				afterScan()
+		cur = mysql.connect().cursor()
+		cur.execute("select * from mugsy.coffeeTypes WHERE upc=(%s)",barcode)
+		r = [dict((cur.description[i][0], value)
+			for i, value in enumerate(row)) for row in cur.fetchall()]
+		return {'coffeeInfo' : r}
+
+
+
+
+
+
+
+
 
 
 
