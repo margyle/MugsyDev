ce5569bee00c770628086fa8b4090f7fd4978018
Matt Argyle <matt.coding@gmail.com>
2019-04-24 20:38:58 -0400

    Add files via upload

---
+++ interface/integrations/# integrationsConfig.py
@@ -0,0 +1,27 @@
+# integrationsConfig.py
+#authKeys grabbed from cloud control config
+
+# todo: multi phone numbers per machine
+smsConfig = {
+    'authKey': '',
+    'phoneNumber': ,
+    'machineId': ''
+}
+
+alexaConfig = {
+    'authKey': '',
+    'machineId': ''
+}
+
+slackConfig = {
+    'authKey': '',
+    'machineId': '',
+    'slackDomain': '',
+}
+
+ifThisThanThatConfig = {
+    'authKey': '',
+    'machineId': '',
+    'endpointUrl': '',    
+}
+
