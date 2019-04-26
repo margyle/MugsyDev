824cb3bb3389524aea3471cc3c674de516ce1527
Matt Argyle <matt.coding@gmail.com>
2019-04-25 21:16:12 -0400

    Update interface.sh

--- runtime/boot/interface.sh
+++ runtime/boot/interface.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 python3 /var/mugsy/decaf/decaf.py 
 chromium-browser --no-sandbox --no-first-run --hide-scrollbars --noerrdialogs --start-fullscreen \
- --start-maximized --disable-notifications --disable-infobars --kiosk http://localhost/mugsy/interface/wifi.php 
+ --start-maximized --disable-notifications --disable-infobars --kiosk http://localhost/mugsy/interface/interfaceHome.php 
