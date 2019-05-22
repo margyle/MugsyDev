05e20857616478a39d48be373d2a6a2896a852da
Matt Argyle <matt.coding@gmail.com>
2019-05-22 13:43:31 -0400

    Update getMachineInfo.php

--- cloudcontrol/api/getMachineInfo.php
+++ cloudcontrol/api/getMachineInfo.php
@@ -1,19 +1,24 @@
 <?php
+$integrationType = 'api_access';
 //todo get all includes into one call
 //todo move to oAuth for API auth
 //include 'vendor/autoload.php';
 include '../inc/inc.db.php';
+//get capi access key from key detective 
+include '../inc/inc.keyDetective.php';
 
-$key = $_GET['userKey'];
+$machineId = $_GET['machineID'];
+$requestingUser = $_GET['userID'];
+$userKey = $_GET['authKey'];
 //debug
 //echo $key;
 
 //api listener
-if (!isset($key)) {
+if ($integrationKey != $userKey) {
     echo "No Permissions";
 } else {
-    $query = $pdo->prepare('SELECT * FROM machines WHERE userId=:parameter');
-    $query->bindParam(':parameter', $key, PDO::PARAM_STR);
+    $query = $pdo->prepare('SELECT * FROM machines WHERE id=:parameter');
+    $query->bindParam(':parameter', $machineId, PDO::PARAM_STR);
     $query->execute();
     //add the fetch for associative array
     $machine = $query->fetch(PDO::FETCH_ASSOC);
