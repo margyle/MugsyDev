136e291e68225a52b5927c8d9f1536ae48da6620
Matt Argyle <matt.coding@gmail.com>
2019-05-23 14:15:00 -0400

    added key detective

--- cloudcontrol/api/getCupTotals.php
+++ cloudcontrol/api/getCupTotals.php
@@ -1,21 +1,24 @@
 <?php
+$integrationType = 'api_access';
 //todo get all includes into one call
 //todo move to oAuth for API auth
+//todo move brew counts from machine settings to user settings(two types of counts)
 //include 'vendor/autoload.php';
 include '../inc/inc.db.php';
+//get capi access key from key detective 
+include '../inc/inc.keyDetective.php';
 
-$key = $_GET['userKey'];
-//$machineId = "foo";
-//debug
-//echo $key;
+$machineId = $_GET['machineID'];
+$requestingUser = $_GET['userID'];
+$userKey = $_GET['authKey'];
 
 //api listener
-if (!isset($key)) {
+if ($integrationKey != $userKey) {
     echo "No Permissions";
 } else {
     //todo convert two queries to one join
     $query = $pdo->prepare('SELECT * FROM machines WHERE userId=:parameter');
-    $query->bindParam(':parameter', $key, PDO::PARAM_STR);
+    $query->bindParam(':parameter', $requestingUser, PDO::PARAM_STR);
     $query->execute();
     //add the fetch for associative array
     $machine = $query->fetch(PDO::FETCH_ASSOC);
