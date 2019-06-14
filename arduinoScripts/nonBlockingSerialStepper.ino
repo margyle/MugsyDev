28133184d5fa6c5cceaed3d46923da3ac7d3052a
Matt Argyle <matt.coding@gmail.com>
2019-06-14 19:34:57 -0400

    Fixed Seria Command Chaining

--- arduinoScripts/nonBlockingSerialStepper.ino
+++ arduinoScripts/nonBlockingSerialStepper.ino
@@ -54,12 +54,11 @@ void loop()
         moveCone();
         newData = false;
     }
-    //blink without delay debug for non block testing/confirmation
     if (currentMillis_1 - previousMillis_1 >= channel1_interval)
     {
-
+        // save the last time you blinked the LED
         previousMillis_1 = currentMillis_1;
-
+        //if the LED is off turn it on and vice-versa:
         if (channel1_state == LOW)
         {
             channel1_state = HIGH;
@@ -68,7 +67,7 @@ void loop()
         {
             channel1_state = LOW;
         }
-        // 
+        // set the LED with the ledState of the variable:
         digitalWrite(channel1, channel1_state);
     }
     stepper1.run();
@@ -157,6 +156,6 @@ void moveCone()
     else
     {
         stepper1.move(-totalSteps);
-      
+        ;
     }
 }
