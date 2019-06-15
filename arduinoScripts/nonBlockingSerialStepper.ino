0387bffdd3da37c512572b3d03ff34f6a73669d2
Matt Argyle <matt.coding@gmail.com>
2019-06-14 20:11:06 -0400

    added additional command for relay control

--- arduinoScripts/nonBlockingSerialStepper.ino
+++ arduinoScripts/nonBlockingSerialStepper.ino
@@ -1,20 +1,20 @@
-//non blocking stepper serial controller but it 
-//has introduced some bugs, need to fix command chaining and reintegrate water pump
+
 #include <AccelStepper.h>
 // Define cone stepper
 AccelStepper stepper1; // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
 
 //define waterPump
+//pump running off relay while testing non-blocking
 const int channel1 = 7;
 int x = 0;
-unsigned long previousMillis_1 = 0;
-const long channel1_interval = 500;
+unsigned long previousMillis_1 = 0; 
+long channel1_interval = 0;
 int channel1_state = LOW;
 
 //set up serial listeners
 const byte numChars = 32;
 char receivedChars[numChars];
-char tempChars[numChars]; // temporary array for use when parsing
+char tempChars[numChars];        // temporary array for use when parsing
 
 // variables to hold the parsed data
 char stepperDirection[numChars] = {0};
@@ -22,96 +22,96 @@ int totalSteps = 0;
 float stepperSpeed = 0.0;
 boolean newData = false;
 
+
 //============
 
-void setup()
-{
-    pinMode(channel1, OUTPUT);
+void setup() {
+    pinMode(channel1, OUTPUT);    
     stepper1.setMaxSpeed(200.0);
     stepper1.setAcceleration(300.0);
     Serial.begin(19200);
     Serial.println("Mugsy Motor Controller: Listening");
-}
+    }
 
 //============
 
-void loop()
-{
+void loop() {
     unsigned long currentMillis_1 = millis();
     unsigned long currentMillis_stepper = millis();
-
-    if (stepper1.distanceToGo() == 0)
-    {
-        //stepper1.moveTo(-stepper1.currentPosition());
-        x = 0;
+    
+    if (stepper1.distanceToGo() == 0 ) {
+        if (newData == true) {
+          strcpy(tempChars, receivedChars);
+          parseData();
+          moveCone();
+          newData = false;
+    }
     }
 
+    //serial listener
     recvWithStartEndMarkers();
-    if (newData == true)
-    {
-        strcpy(tempChars, receivedChars);
-        parseData();
-        moveCone();
-        newData = false;
+    if (stepper1.distanceToGo() == 0 ) {
+        if (newData == true) {
+          strcpy(tempChars, receivedChars);
+          parseData();
+          moveCone();
+          newData = false;
     }
-    if (currentMillis_1 - previousMillis_1 >= channel1_interval)
-    {
+    }
+
+//water pump relay
+    if(channel1_interval!=0 && stepper1.distanceToGo() != 0 ){
+    if (currentMillis_1 - previousMillis_1 >= channel1_interval) {
         // save the last time you blinked the LED
         previousMillis_1 = currentMillis_1;
-        //if the LED is off turn it on and vice-versa:
-        if (channel1_state == LOW)
-        {
-            channel1_state = HIGH;
-        }
-        else
-        {
-            channel1_state = LOW;
+    //if the LED is off turn it on and vice-versa:
+        if (channel1_state == LOW) {
+        channel1_state = HIGH;
+        } else {
+        channel1_state = LOW;
         }
         // set the LED with the ledState of the variable:
         digitalWrite(channel1, channel1_state);
+    }   
     }
-    stepper1.run();
-    //  Serial.println(stepper1.currentPosition());
-    //  Serial.println(".");
-    //  Serial.println(stepper1.distanceToGo());
+
+  
+  stepper1.run();
+//  Serial.println(stepper1.currentPosition()); 
+//  Serial.println(".");
+//  Serial.println(stepper1.distanceToGo());
+
+
+
 }
 
 //============
 
-void recvWithStartEndMarkers()
-{
+void recvWithStartEndMarkers() {
     static boolean recvInProgress = false;
     static byte ndx = 0;
     char startMarker = '<';
     char endMarker = '>';
     char rc;
 
-    while (Serial.available() > 0 && newData == false)
-    {
+    while (Serial.available() > 0 && newData == false) {
         rc = Serial.read();
-
-        if (recvInProgress == true)
-        {
-            if (rc != endMarker)
-            {
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
                 receivedChars[ndx] = rc;
                 ndx++;
-                if (ndx >= numChars)
-                {
+                if (ndx >= numChars) {
                     ndx = numChars - 1;
                 }
             }
-            else
-            {
+            else {
                 receivedChars[ndx] = '\0'; // terminate the string
                 recvInProgress = false;
                 ndx = 0;
                 newData = true;
             }
         }
-
-        else if (rc == startMarker)
-        {
+        else if (rc == startMarker) {
             recvInProgress = true;
         }
     }
@@ -119,26 +119,28 @@ void recvWithStartEndMarkers()
 
 //============
 
-void parseData()
-{                     // split the data into its parts
-    char *strtokIndx; // this is used by strtok() as an index
+void parseData() {      // split the data into its parts
+    char * strtokIndx; // this is used by strtok() as an index
 
-    strtokIndx = strtok(tempChars, ",");  // get the first part - the string
+    strtokIndx = strtok(tempChars,",");      // get the first part - the string
     strcpy(stepperDirection, strtokIndx); // copy it to stepperDirection
-
+ 
     strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-    totalSteps = atoi(strtokIndx);  // convert this part to an integer
+    totalSteps = atoi(strtokIndx);     // convert this part to an integer
 
     strtokIndx = strtok(NULL, ",");
-    stepperSpeed = atof(strtokIndx); // convert this part to a float
+    stepperSpeed = atof(strtokIndx);     // convert this part to a float
+    //get pump relay timing
+    strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+    channel1_interval = atoi(strtokIndx);     // convert this part to an integer
+
 }
 
 //============
 //serial command format:
-///<CW,1600,60.00>
+///<CW,400,60.00><CC,400,60.00>
 //<CW,800,60.00><CC,800,60.00><CW,800,60.00><CC,1600,60.00><CW,1600,60.00>
-void moveCone()
-{
+void moveCone() {
     Serial.print("Command Recieved: ");
     Serial.println(receivedChars);
     Serial.print("Direction: ");
@@ -147,15 +149,18 @@ void moveCone()
     Serial.println(totalSteps);
     Serial.print("Speed: ");
     Serial.println(stepperSpeed);
-    Serial.println(stepper1.isRunning());
+    Serial.print("Water Pump Interval: ");
+    Serial.println(channel1_interval);
+  
 
-    if (strcmp(stepperDirection, "CW") != 0)
-    {
-        stepper1.move(totalSteps);
+    if (strcmp(stepperDirection,"CW")!=0){
+    stepper1.move(totalSteps);}
+    else {
+    stepper1.move(-totalSteps);;
     }
-    else
-    {
-        stepper1.move(-totalSteps);
-        ;
-    }
+
+
 }
+
+
+
