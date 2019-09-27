15c95226aebb28cb8670b6ccaf335747d19e9cf7
Matt Argyle <matt.coding@gmail.com>
2019-09-27 17:37:05 -0400

    Create interface.sh

---
+++ release/helpers/interface.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+python3 /var/mugsy/decaf.py
+python3 /var/mugsy/mugsyOS.py
+chromium-browser --no-sandbox --no-first-run --hide-scrollbars --noerrdialogs --start-fullscreen \
+ --start-maximized --disable-notifications --disable-infobars --kiosk http://localhost/mugsy/interface/wifi.php
