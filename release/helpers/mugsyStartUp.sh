f1cd0a1db490a3eb1a91bb9f4032c845487eef28
Matt Argyle <matt.coding@gmail.com>
2019-09-27 17:45:30 -0400

    edit to open in a secure connection

--- release/helpers/mugsyStartUp.sh
+++ release/helpers/mugsyStartUp.sh
@@ -2,4 +2,4 @@
 python3 /var/mugsy/decaf.py
 python3 /var/mugsy/mugsyOS.py
 chromium-browser --no-sandbox --no-first-run --hide-scrollbars --noerrdialogs --start-fullscreen \
- --start-maximized --disable-notifications --disable-infobars --kiosk http://localhost/mugsy/interface/wifi.php
+ --start-maximized --disable-notifications --disable-infobars --kiosk https://localhost/mugsy/interface/wifi.php
