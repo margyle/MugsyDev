ca5a5b352d30f738f0a6a0f124c2c67f292a5353
Matt Argyle <matt.coding@gmail.com>
2019-11-11 13:05:41 -0500

    comment clean up
    added comment breaks to simplify editing for new coders

--- release/commander/mugsyCommander.ino
+++ release/commander/mugsyCommander.ino
@@ -1,8 +1,12 @@
+//--------------------------
+//Library Includes
 #include <AccelStepper.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+//--------------------------
 
 
+//--------------------------
 //Temp sensor
 #define ONE_WIRE_BUS 3
 OneWire oneWire(ONE_WIRE_BUS);
@@ -11,50 +15,81 @@ int deviceCount = 0;
 float temp1;
 float temp2;
 float averageTemp;
+//--------------------------
 
+
+
+//--------------------------
 //weight sensor
 #include "HX711.h"
 #define DOUT  A0
 #define CLK  A1
 #define calibration_factor 246000 
 HX711 scale;
+//--------------------------
 
+
+
+//--------------------------
 // Define cone stepper
 AccelStepper stepper1; // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+//--------------------------
 
+
+
+//--------------------------
 //define waterPump
 const int channel1 = 7;
 int x = 0;
 unsigned long previousMillis_1 = 0; 
 long channel1_interval = 0;
 int channel1_state = LOW;
+//--------------------------
 
+
+
+//--------------------------
 //set up serial listeners
 const byte numChars = 32;
 char receivedChars[numChars];
-char tempChars[numChars];        // temporary array for use when parsing
+// temporary array for use when parsing
+char tempChars[numChars];        
+//--------------------------
 
+
+
+//--------------------------
 // variables to hold the parsed data
 char queryType[numChars] = {0};
 int stepperDirection = 0;
 int totalSteps = 0;
 float stepperSpeed = 0.0;
 boolean newData = false;
+//--------------------------
 
-//weight
+
+
+//--------------------------
+//weight variables
 float weight = 0.00;
+//--------------------------
 
+
+
+//--------------------------
 //queryTypes
 char moveQuery[] ="M";
 char tempQuery[] ="T";
 char weightQuery[] ="W";
 //(WiFi.SSID() == ReadWifiSSID())
+//--------------------------
 
 
-//============
 
+//-------------------------- START SETUP
 void setup() {
     pinMode(channel1, OUTPUT);
+    
     //set up temp sensors
     sensors.begin();   
     deviceCount = sensors.getDeviceCount();
@@ -64,35 +99,37 @@ void setup() {
     scale.set_scale(calibration_factor); 
     //Assuming there is no weight on the scale at start up, reset the scale to 0 
     scale.tare();
+    
     //set up stepper
     stepper1.setMaxSpeed(200.0);
     stepper1.setAcceleration(300.0);
+    
     //let Mugsy know system is online
     Serial.begin(19200);
     Serial.println("200");
     Serial.println("Mugsy Commander: Listening");
-    //Serial.println("Mugsy Motor Controller: Listening");
     }
 
-//============
+//-------------------------- END SETUP
 
+
+
+//-------------------------- START LOOP
 void loop() {
-    unsigned long currentMillis_1 = millis();
-    unsigned long currentMillis_stepper = millis();
-    
-    if (stepper1.distanceToGo() == 0) {
-        if (newData == true) {
-          strcpy(tempChars, receivedChars);
-          parseData();
-          newData = false;
-
-    }
-    }
+  unsigned long currentMillis_1 = millis();
+  unsigned long currentMillis_stepper = millis();
+  
+  if (stepper1.distanceToGo() == 0) {
+    if (newData == true) {
+      strcpy(tempChars, receivedChars);
+      parseData();
+      newData = false;
+   }
+   }
 
 //serial listener
 recvWithStartEndMarkers();
 
-//============
 //water pump relay, currently running by steps remaining, need to convert to time/ml
 if(channel1_interval!=0 && stepper1.distanceToGo() != 0 ){
   if (currentMillis_1 - previousMillis_1 >= channel1_interval) {
@@ -107,11 +144,13 @@ if(channel1_interval!=0 && stepper1.distanceToGo() != 0 ){
 }
 
 stepper1.run();
-
 }
 
-//============
+//-------------------------- END LOOP
 
+
+
+//-------------------------- START recvWithStartEndMarkers
 void recvWithStartEndMarkers() {
     static boolean recvInProgress = false;
     static byte ndx = 0;
@@ -142,30 +181,35 @@ void recvWithStartEndMarkers() {
     }
 }
 
-//============
+//-------------------------- END recvWithStartEndMarkers
 
-void parseData() {      // split the data into its parts
-    char * strtokIndx; // this is used by strtok() as an index
 
-    //get query type
-    strtokIndx = strtok(tempChars,","); // get the first part - the string
-    strcpy(queryType, strtokIndx);
+
+//-------------------------- START parseData 
+// split the data into its parts
+void parseData() {      
+  char * strtokIndx; 
+
+//get query type
+  strtokIndx = strtok(tempChars,","); // get the first part - the string
+  strcpy(queryType, strtokIndx);
     
-    //if requesting temp
-    if(queryType[0] == tempQuery[0]){
-        getTemp();
-        newData = false;
-     }
-     //if requesting weight
-     else if(queryType[0] == weightQuery[0]){
-      getWeight();
-      newData = false;
-     }
-     
-     
-    else if (queryType[0] == moveQuery[0]){
+//if requesting temp
+  if(queryType[0] == tempQuery[0]){
+    getTemp();
+    newData = false;
+    }
     
-    //get direction:0=CW,1=CC
+//if requesting weight
+  else if(queryType[0] == weightQuery[0]){
+    getWeight();
+    newData = false;
+    }
+    
+ //if requesting cone movement
+  else if (queryType[0] == moveQuery[0]){
+    
+//get direction:0=CW,1=CC
     strtokIndx = strtok(NULL, ","); // get the first part - the string
     stepperDirection = atoi(strtokIndx); // copy it to stepperDirection
  
@@ -174,34 +218,37 @@ void parseData() {      // split the data into its parts
 
     strtokIndx = strtok(NULL, ",");
     stepperSpeed = atof(strtokIndx);     // convert this part to a float
+    
     //get pump relay timing
     strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
     channel1_interval = atoi(strtokIndx);     // convert this part to an integer
     moveCone();
     coneResponse();
-        }
+    }
 }
-//============
+//-------------------------- END parseData
 
-//============
+
+
+
+//-------------------------- START getTemp
 void getTemp(){
    sensors.requestTemperatures(); 
    temp1 = sensors.getTempCByIndex(0);
    temp2 = sensors.getTempCByIndex(1);
    averageTemp = ((temp1 + temp2)/2); 
    Serial.println(averageTemp);
-   //debug
-//   Serial.print("Sensor1: ");
-//   Serial.println(temp1);
-//   Serial.print("Sensor2: ");
-//   Serial.println(temp2);
-
-   
-     
+//debug
+  //   Serial.print("Sensor1: ");
+  //   Serial.println(temp1);
+  //   Serial.print("Sensor2: ");
+  //   Serial.println(temp2);
 }
-//============
+//--------------------------  END getTemp
 
-//============
+
+
+//-------------------------- START moveCone
 //move cone command structure
 //<M,0,800,60.00>
 //<M,Direction, Steps, Speed
@@ -213,6 +260,11 @@ void moveCone() {
     }
 
 }
+//-------------------------- END moveCone
+
+
+
+//-------------------------- START coneResponse
 void coneResponse() {
     Serial.print("Command Recieved: ");
     Serial.println(receivedChars);
@@ -232,6 +284,11 @@ void coneResponse() {
 //    Serial.println(moveQuery);
 }
 
+//-------------------------- END coneResponse
+
+
+
+//-------------------------- START getWeight
 void getWeight(){
   weight = scale.get_units() * 1000;
   if (weight * weight <= 4.00){
@@ -242,3 +299,4 @@ void getWeight(){
       Serial.println(" grams");
   }
 }
+//-------------------------- END get Weight
