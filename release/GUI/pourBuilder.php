95a6f61cabf660da83c6c9f20db78cae3a0159d9
Matt Argyle <matt.coding@gmail.com>
2019-12-05 00:00:19 -0500

    added stickyList and optionEditor Wrapper

--- release/GUI/pourBuilder.php
+++ release/GUI/pourBuilder.php
@@ -4,8 +4,7 @@ session_start();
 //include 'vendor/autoload.php'; #TODO prep local oAuth 
 include 'inc/inc.db.php';
 include 'inc/inc.getRecipes.php';
-include 'inc/inc.createRecipe.php';
-
+include 'inc/inc.getMachineStatus.php';
 ?>
 <!DOCTYPE html>
 <html lang="">
@@ -17,58 +16,8 @@ include 'inc/inc.createRecipe.php';
     <title>Mugsy</title>
 
     <!-- Bootstrap CSS -->
-
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
-    <style>
-        html {
-            overflow: scroll;
-            overflow-x: hidden;
-        }
-
-        ::-webkit-scrollbar {
-            width: 0px;
-            /* remove scrollbar space */
-            background: transparent;
-            /* optional: just make scrollbar invisible */
-        }
-
-        /* optional: show position indicator in red */
-        ::-webkit-scrollbar-thumb {
-            background: #FF0000;
-        }
-
-        body {
-            background-image: url("img/themes/purp/background.png");
-            background-repeat: repeat-x;
-            background-attachment: fixed;
-            color: #707070;
-            overflow-x: hidden;
-        }
-
-        @font-face {
-            font-family: bebas;
-
-            src: url(inc/webfonts/Bebas/BEBAS.ttf);
-        }
-
-        @font-face {
-            font-family: avenir;
-
-            src: url(inc/webfonts/avenir/AvenirLTStd-Roman.woff);
-        }
-
-        @font-face {
-            font-family: avenirOblique;
-
-            src: url(inc/webfonts/avenir/AvenirLTStd-Oblique.woff);
-        }
-    </style>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
@@ -80,87 +29,112 @@ include 'inc/inc.createRecipe.php';
                 <span style="font-family: bebas;font-size: 17pt;color: #707070;">Pour Builder</span>
             </div>
             <div class="col-md-12" style="padding-top: 8px">
-
-
                 <div class="progress shadow" id="prog" style="height: 20px;">
                     <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">Step 1
                     </div>
                 </div>
             </div>
-
-
         </div>
 
         <div class="row">
             <div class="col-md-12" style="padding-top: 24px;">
 
-                <ul class="list-group list-group-horizontal shadow" id="drag1" style="width:100%;">
+                <ul class="list-group list-group-horizontal shadow" id="availableOptions" style="width:100%;">
 
-                    <li class="list-group-item list-group-item-danger text-center" style="background-color:#f28c8f" id="CW">
-
-
+                    <li class="list-group-item list-group-item-danger text-center" style="background-color:#f28c8f" id="grind">
                         <span style="font-size: 1em;color: white;">grind
                             <i class="fas fa-sun" style="size: 4em;color: white;"></i>
-
                         </span>
-
                     </li>
 
-                    <li class="list-group-item list-group-item-secondary text-center" style="background-color: #7c75b2;color:white;" id="CW">
-
-
+                    <li class="list-group-item list-group-item-secondary text-center" style="background-color: #7c75b2;color:white;" id="cone">
                         <span style="font-size: 1em;">cone
                             <i class="fas fa-circle-notch" style="size: 4em"></i>
-
                         </span>
-
                     </li>
-                    <li class="list-group-item list-group-item-primary text-center" style="background-color: #00b7ee;color:white;">
 
+                    <li class="list-group-item list-group-item-primary text-center" style="background-color: #00b7ee;color:white;" id="water">
                         <span style="font-size: 1em; ">water
                             <i class="fas fa-tint" style="size: 4em"></i>
-
                         </span>
                     </li>
 
-                    <li class="list-group-item list-group-item-success text-center" style="background-color: #2ab071;color: white;">
-
+                    <li class="list-group-item list-group-item-success text-center" style="background-color: #2ab071;color: white;" id="spout">
                         <span style="font-size: 1em; ">spout
                             <i class="fas fa-grip-lines" style="size: 4em"></i>
-
                         </span>
                     </li>
 
-                    <li class="list-group-item list-group-item-dark text-center">
-
+                    <li class="list-group-item list-group-item-dark text-center" id="time">
                         <span style="font-size: 1em; ">time
                             <i class="fas fa-clock" style="size: 4em"></i>
-
                         </span>
                     </li>
-                    <li class="list-group-item list-group-item-dark text-center"">
 
+                    <li class="list-group-item list-group-item-dark text-center" id="repeat">
                         <span style=" font-size: 1em; ">repeat
                             <i class=" fas fa-redo-alt" style="size: 4em"></i>
-
                         </span>
                     </li>
-                    <li class="list-group-item list-group-item-dark text-center">
 
+                    <li class="list-group-item list-group-item-dark text-center" id="stop">
                         <span style="font-size: 1em; ">stop
                             <i class="fas fa-stop-circle" style="size: 4em"></i>
-
                         </span>
                     </li>
-
                 </ul>
 
             </div>
 
         </div>
+        <!--end available steps -->
+
+        <div class="row">
+            <!--start anchor list and editors-->
+            <div class="col-md-4" style="padding-top: 24px; padding-bottom:20px;">
+                <div class="card shadow">
+                    <div class="card-header text-center">
+                        Step Builder
+                    </div>
+                    <!--drag step options to this sticky list-->
+                    <ul class="list-group list-group-flush this-list" id="stepStickyList">
+                        <li class="list-group-item list-group-item-danger text-center" id="grind" style="background-color: #f28c8f;color: white;">
+                            <span style="font-size: 1em;">grind
+                                <i class="fas fa-sun" style="size: 4em"></i>
+                            </span>
+                        </li>
+                    </ul>
+                    <!--end sticky list-->
+                </div>
+            </div>
+            <div class="col-md-8 " style="padding-top: 24px;padding-bottom: 20px">
+                <div id="stepOptionEditor">
+                    <div class="col-md-14">
+                        <div class="card shadow">
+                            <div class="card-header" id="editorHeader" style="background: #00b7ee;color: white;">
+                                Step Editor:
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text"><span id="editorActionable">
+                                    </span></p>
+                                </span></p>
+                                <button type="button" class="btn list-group-item-success " onClick="addStep()" style="background-color: #2ab071;color: white;">Next
+                                    Step</button>
+                                <button type="button" class="btn list-group-item-danger " onClick="deleteItem();" style="background-color:#f28c8f;color:white;">Delete
+                                    Step</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!--end anchor list and editors-->
+
 
     </div>
-    </div>
+    <!--end container-->
+
 
 
 </body>
@@ -184,10 +158,11 @@ include 'inc/inc.createRecipe.php';
 
 
 <script>
+    var setting = 1;
+    //make available step options draggable
     $(document).ready(function() {
-        Sortable.create(drag1, {
+        Sortable.create(availableOptions, {
             animation: 100,
-
             group: {
                 name: "list-1",
                 pull: "clone",
@@ -199,36 +174,29 @@ include 'inc/inc.createRecipe.php';
             sort: false,
             filter: '.sortable-disabled',
             //chosenClass: 'active',
-
-
         });
 
-        Sortable.create(deleteID, {
+        //make sticky list to recieve step options
+        Sortable.create(stepStickyList, {
             group: 'list-1',
             handle: '.list-group-item',
             onAdd(evt) {
 
-                mainDrag();
+                //updateEditor();
                 setting++
 
             }
         });
     });
-</script>
-
-
-<script>
+    //function to delete last step option
     function deleteItem() {
-        var count = document.getElementById('deleteID').getElementsByTagName("li");
-
+        var count = document.getElementById('stepStickyList').getElementsByTagName("li");
         var i = count.length
-        var ulElem = document.getElementById('deleteID')
-
+        var ulElem = document.getElementById('stepStickyList')
         ulElem.removeChild(ulElem.childNodes[i])
         setting--;
-        mainDrag();
+        //mainDrag();
     }
 </script>
-
 
 </html>
